34128606c944e3a975fb38f3697f7e8a9cbaf829
SarahFohse <sarahfohse@gmail.com>
2023-09-28 14:14:39 +0100

    Javascript additions and fixes
    dynamically adding divs, images and headings with Javascript

--- js/main.js
+++ js/main.js
@@ -23,21 +23,23 @@ const emailInput = document.getElementById('email');
 let contain = document.getElementById('contain'); 
 let addImage= document.getElementById('addimg');
 
-
-
-addEmail.addEventListener('click', (event) => {
+addEmail.addEventListener('click', event => {
   event.preventDefault();
-  let user = document.createElement('div');
-  user.id = `user` + `=${Math.random().toString().split('.')[1]}`
-  contain.appendChild(user);
-  return addEmail;
-  // user.document.createElement('p');
 
-  // if (user.id = true) {
-  //   let userName = document.createElement('p');
-  //   user.appendChild(`${emailInput}`)
-  // }
-})
+  if (emailInput.value != '' && document.getElementById(emailInput.value) == null) {
+    let user = document.createElement('div');
+    user.id = emailInput.value;
+    user.classList.add('user-container');
+    contain.appendChild(user);
+
+    //heading
+    let h3 = document.createElement('h3');
+    let textNode = document.createTextNode(emailInput.value);
+    h3.appendChild(textNode);
+    user.appendChild(h3);
+  }
+});
+
 
 
 //add image creates img in html for user div
@@ -45,5 +47,10 @@ addEmail.addEventListener('click', (event) => {
 addImage.addEventListener('click', () => {
   let newImage = document.createElement('img');
   newImage.src = image.src;
-  document.getElementById('user').appendChild(newImage);
+  document.getElementById(emailInput.value).appendChild(newImage);
 })
+
+
+
+
+
