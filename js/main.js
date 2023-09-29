b0e96599b8a4f0bc89a3ae7c875d2714f3b90d74
SarahFohse <sarahfohse@gmail.com>
2023-09-29 14:01:59 +0100

    Working javascript array w/out validation yet

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@
 //// generating a random image on click of the get image button
 let getImage = document.getElementById('getimg');
 let url = `https://picsum.photos/200/300?random`;
+let currentEmail;
+let userList = [];
+let userNumber;
 
 let image = document.getElementById('img');
 
@@ -16,42 +19,84 @@ getImage.addEventListener('click', () => {
   return getImage;
 })
 
-////email creates div for user
+////email creates div for user and checks if an email has been entered
 
 let addEmail = document.getElementById('addmail');
 const emailInput = document.getElementById('email');
 let contain = document.getElementById('contain'); 
 let addImage= document.getElementById('addimg');
+const displayEmail = document.getElementById('error-email');
+
 
 addEmail.addEventListener('click', event => {
   event.preventDefault();
+  currentEmail = emailInput.value;
+  displayEmail.innerText = `Current email: ${currentEmail == '' ? 'No email entered.' : currentEmail}`;
+  displayEmail.style.display = 'block';
+  let emailExists = false;
+  userList.forEach((item) => {
+    if (item.email == currentEmail) {
+      emailExists = true;
+    }
+  });
 
-  if (emailInput.value != '' && document.getElementById(emailInput.value) == null) {
+
+  if (emailInput.value != '' && !emailExists) {
+    userNumber = userList.length;
+    userList.push({email: currentEmail, id: `user${Math.random().toString().split('.')[1]}`});
+
     let user = document.createElement('div');
-    user.id = emailInput.value;
+    user.id = userList[userNumber].id;
     user.classList.add('user-container');
     contain.appendChild(user);
+
 
     //heading
     let h3 = document.createElement('h3');
     let textNode = document.createTextNode(emailInput.value);
     h3.appendChild(textNode);
     user.appendChild(h3);
+    errorMessage.style.display = 'none';
   }
 });
 
 
 
-//add image creates img in html for user div
+//add image creates img in html for user div and checks if the image has already been linked to the user as it shouldn't display twice
+const errorMessage = document.getElementById('error-message');
 
 addImage.addEventListener('click', () => {
+  let imageExist = false;
+  let userID;
+
+  userList.forEach((item, i) => {
+    if (item.email == currentEmail) {
+      userNumber = i;
+    }
+  });
+
+  if (userList.length > 0) {
+    userID = document.querySelectorAll(`#${userList[userNumber].id} img`);
+
+    userID.forEach(img => {
+      if (img.src == image.src) {
+        imageExist = true;
+      }
+    })
+  }
   
-  let newImage = document.createElement('img');
-  newImage.src = image.src;
-  document.getElementById(emailInput.value).appendChild(newImage);
+  if (imageExist === true) {
+    errorMessage.innerText = 'Image already added!';
+    errorMessage.style.display = 'block';
+
+  } else if (imageExist === false && currentEmail !== undefined && userList.length > 0) {
+    let newImage = document.createElement('img');
+    newImage.src = image.src;
+    document.getElementById(userList[userNumber].id).appendChild(newImage);
+    errorMessage.style.display = 'none';
+  } else {
+    errorMessage.innerText = 'Enter an email address!';
+    errorMessage.style.display = 'block';
+  }
 })
-
-
-
-
 
