bf9bc56b558b7a2232a799ce2566b45793e29ad5
SarahFohse <sarahfohse@gmail.com>
2023-10-03 10:26:30 +0100

    changes to javascript and preparing for scss

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ const emailInput = document.getElementById('email');
 let contain = document.getElementById('contain'); 
 let addImage= document.getElementById('addimg');
 const displayEmail = document.getElementById('error-email');
-let clearUser 
-
+let clearUser;
+const clearAll = document.getElementById('clear-all');
 
 addEmail.addEventListener('click', event => {
   event.preventDefault();
@@ -72,6 +72,10 @@ addEmail.addEventListener('click', event => {
     clearUser.textContent = 'Clear User';
     clearUser.classList.add('clear-btn')
     user.appendChild(clearUser);
+
+    if (clearAll.style.display == 'none') {
+      clearAll.style.display = 'block';
+    }
   }
 });
 
@@ -116,10 +120,12 @@ const validateInputs = () => {
 
 //add image creates img in html for user div and checks if the image has already been linked to the user as it shouldn't display twice
 const errorMessage = document.getElementById('error-message');
+let maxImages = 15;
 
 addImage.addEventListener('click', () => {
   let imageExist = false;
   let userID;
+  let userImageContainer;
 
   userList.forEach((item, i) => {
     if (item.email == currentEmail) {
@@ -127,7 +133,8 @@ addImage.addEventListener('click', () => {
     }
   });
 
-  if (userList.length > 0) {
+  if (userList[userNumber] != undefined) {
+    userImageContainer = document.getElementById(userList[userNumber].id);
     userID = document.querySelectorAll(`#${userList[userNumber].id} img`);
 
     userID.forEach(img => {
@@ -141,18 +148,61 @@ addImage.addEventListener('click', () => {
     errorMessage.innerText = 'Image already added!';
     errorMessage.style.display = 'block';
 
-  } else if (imageExist === false && currentEmail !== undefined && userList.length > 0 && validationCheck === true) {
+  } else if (imageExist === false && currentEmail !== undefined && userList.length > 0 && validationCheck === true && userImageContainer !== undefined && userImageContainer.querySelectorAll('img').length < maxImages) {
     let newImage = document.createElement('img');
     newImage.src = image.src;
-    document.getElementById(userList[userNumber].id).appendChild(newImage);
+    userImageContainer.appendChild(newImage);
     errorMessage.style.display = 'none';
+  } else if (userImageContainer !== undefined && userImageContainer.querySelectorAll('img').length == maxImages) {
+    //was alert, now error message. Might change
+    errorMessage.innerText = 'You reached the limit of 15 images.';
+    errorMessage.style.display = 'block';
   } else {
     errorMessage.innerText = 'Click on - add your email address!';
     errorMessage.style.display = 'block';
   }
 })
 
-clearUser.addEventListener('click', () => {
-  const removeAll = document.getElementsByClassName('user-container');
-  removeAll[0].remove()
-})
+// Add click event listener to document, pass event so we can check clicked item
+document.addEventListener("click", function(e){
+  const clearUserBtn = e.target.closest(".clear-btn");
+
+  if(clearUserBtn){
+    for (let i = 0; i < userList.length; i++) {
+      if (userList[i].id == clearUserBtn.parentElement.id) {
+        userList.splice(i, 1);
+      }
+    }
+
+    if (clearUserBtn.previousElementSibling.innerText == currentEmail) {
+      currentEmail = '';
+      emailInput.value = '';
+      displayEmail.innerText = 'Current email: No email entered.';
+    }
+
+    clearUserBtn.parentElement.remove();
+
+    if (userList.length == 0) {
+      clearAll.style.display = 'none';
+    }
+  }
+});
+
+clearAll.addEventListener('click', () => {
+  let removeAll = document.querySelectorAll('.user-container');
+
+  removeAll.forEach(user => {
+    for (let i = 0; i < userList.length; i++) {
+      if (userList[i].id == user.id) {
+        userList.splice(i, 1);
+      }
+    }
+
+    user.remove();
+  });
+
+  currentEmail = '';
+  emailInput.value = '';
+  displayEmail.innerText = 'Current email: No email entered.';
+  clearAll.style.display = 'none';
+});
